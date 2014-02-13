8274d09e7e55130b8155613a6e4111ae34427318
Lynnette Ng <lynnette-ng@outlook.com>
2014-02-13 15:06:58 +0800

    Added the separate packages of our architecture

---
+++ src/com/nus/TaskPad/execute/Executor.java
@@ -0,0 +1,5 @@
+package com.nus.TaskPad.execute;
+
+public class Executor {
+
+}
---
+++ src/com/nus/TaskPad/inputproc/Input.java
@@ -0,0 +1,10 @@
+package com.nus.TaskPad.inputproc;
+
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
