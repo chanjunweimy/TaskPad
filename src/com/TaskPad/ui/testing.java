48c49a426878f3c018f40a2d575f3ead9b4734b7
Lynnette Ng <lynnette-ng@outlook.com>
2014-02-14 09:45:46 +0800

    For Junwei

--- src/com/TaskPad/ui/testing.java
+++ src/com/TaskPad/ui/testing.java
@@ -1,30 +1,25 @@
+package com.TaskPad.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.LineBorder;
 
-
-public class test2 {
+public class testing {
 
 	public static void main(String[] args) {
-		new test2();
+		new testing();
 	}
 
-	public test2() {
+	public testing() {
 		final double COMPUTER_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		final double COMPUTER_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 		
@@ -39,7 +34,7 @@ public class test2 {
 				JFrame frame = new JFrame("Testing");
 				frame.setUndecorated(true);//to hide the top level container
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.add(new InputPanel());
+				frame.add(new TestPane());
 				frame.pack();
 				frame.setLocation((int)(COMPUTER_WIDTH/2.5),(int)(COMPUTER_HEIGHT/10.0));
 				//frame.setLocationRelativeTo(null);//make centre
@@ -48,40 +43,38 @@ public class test2 {
 			}
 		});
 	}
-	
-	public class InputPanel extends JPanel {
+
+	public class TestPane extends JPanel {
 
 		/**
 		 * 
 		 */
 		private static final long serialVersionUID = 1L;
-		private JTextField txtbox = new JTextField(20);
-		//private JTextArea output = new JTextArea(5, 15);
+		private JList<CharSequence> list;
+		private DefaultListModel<CharSequence> model;
 
-		public InputPanel() {
+		public TestPane() {
 			setLayout(new BorderLayout());
 			setBorder(new LineBorder(Color.BLACK));
-			
-			
-			TextFieldListener tfListener = new TextFieldListener();
-			txtbox.addActionListener(tfListener);
-			txtbox.requestFocus();
-			txtbox.setLocation(0, 0);
-			
-			//output.setEditable(false);
-			//output.setLocation(0, -1000);
-			
-			add(txtbox);
-			//add(output);
+			model = new DefaultListModel<CharSequence>();
+			list = new JList<CharSequence>(model);
+			//list.setBounds(-150, -1000, 220, 150);
+			list.setMaximumSize(new Dimension(Integer.MAX_VALUE,
+					Integer.MAX_VALUE));
+			doAction();
+
+			add(list);
 		}
-	
-	// The listener for the textfield.
-		private class TextFieldListener implements ActionListener
-		{  public void actionPerformed(ActionEvent evt)
-		{  String inputString = txtbox.getText();
-		//output.append(inputString + "\n");
-		txtbox.setText("");
-		}
+
+		public void doAction() {
+
+			String s = "random";
+
+			if (!model.isEmpty()) {
+				model.clear();
+			}
+
+			model.addElement(s);
 		}
 	}
 }
