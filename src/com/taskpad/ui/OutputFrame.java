2dcf5742f3aa4d2c12dd49029b1f2501a3428695
Chan Jun Wei <chanjunweimy@gmail.com>
2014-02-23 15:10:36 +0800

    solved some warnings in Add.java

---
+++ src/com/taskpad/ui/OutputFrame.java
@@ -0,0 +1,56 @@
+package com.taskpad.ui;
+
+import java.awt.Color;
+import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class OutputFrame extends JFrame{
+
+	/**
+	 * default
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final double COMPUTER_WIDTH = 
+			Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+	private final double COMPUTER_HEIGHT = 
+			Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+	private final Color OUTPUTBOX_BORDER_COLOR = 
+			new Color(112, 48, 160);
+	private final Color OUTPUTBOX_BACKGROUND_COLOR = 
+			new Color(242, 242, 242);
+	
+	protected final static int OUTPUTFRAME_WIDTH = 300;
+	protected final static int OUTPUTFRAME_HEIGHT = 150;
+	protected static JTextArea output = new JTextArea(5, 15);
+	
+	public OutputFrame(){
+		initialOutputBox();
+		initialOutputFrame();
+	}
+
+	private void initialOutputFrame() {
+		//make JFrame Disappear
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setSize(OUTPUTFRAME_WIDTH,OUTPUTFRAME_HEIGHT);
+		setLocation((int)(COMPUTER_WIDTH/2),
+					(int)(COMPUTER_HEIGHT/2 - OUTPUTFRAME_HEIGHT));
+		
+		this.getContentPane().add(output);
+		
+		setVisible(true);
+	}
+
+	private void initialOutputBox() {
+		// Don't let the user change the output.
+		output.setEditable(false);
+		output.setBackground(OUTPUTBOX_BACKGROUND_COLOR);
+		output.setBorder(BorderFactory.createLineBorder(OUTPUTBOX_BORDER_COLOR));
+	}
+
+}
---
+++ src/com/taskpad/ui/TaskPadMain.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Chan Jun Wei, Lynnette Ng Hui Xian, Wang Taining
+ * Product: TaskPad
+ * Team: W13-3j
+ */
+
+package com.taskpad.ui;
+
+import com.taskpad.inputproc.InputMain;
+
+public class TaskPadMain{
+	public static void main(String[] args){
+		new InputMain();
+		new InputFrame();
+		new OutputFrame();
+	}
+}
