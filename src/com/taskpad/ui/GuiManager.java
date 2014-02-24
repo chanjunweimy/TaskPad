cb687aaa59d3388e9ae84d9ddfaa3dd544f95c1c
Chan Jun Wei <chanjunweimy@gmail.com>
2014-02-25 00:13:44 +0800

    done some refactoring and started implementing hotkeys

--- src/com/taskpad/ui/GuiManager.java
+++ src/com/taskpad/ui/GuiManager.java
@@ -3,11 +3,25 @@ package com.taskpad.ui;
 import com.taskpad.inputproc.InputManager;
 
 public class GuiManager {
+	private static InputFrame _inputFrame;
+	private static OutputFrame _outputFrame;
+	
 	public GuiManager(){
 	}
 	
+	public GuiManager(InputFrame inputFrame, OutputFrame outputFrame){
+		initialGuiManager(inputFrame, outputFrame);
+	}
+
+	private void initialGuiManager(InputFrame inputFrame,
+			OutputFrame outputFrame) {
+		setInputFrame(inputFrame);
+		setOutputFrame(outputFrame);
+	}
+	
 	public static void callExit(){
-		System.exit(0);
+		_inputFrame.close();
+		_outputFrame.close();
 	}
 	
 	public static void callOutput(String out){
@@ -16,5 +30,13 @@ public class GuiManager {
 	
 	public static void passInput(String in){
 		InputManager.receiveFromGui(in);
+	}
+
+	private void setInputFrame(InputFrame _inputFrame) {
+		GuiManager._inputFrame = _inputFrame;
+	}
+
+	public void setOutputFrame(OutputFrame _outputFrame) {
+		GuiManager._outputFrame = _outputFrame;
 	}
 }
