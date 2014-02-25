6421d8690c212fc9415247a72916348f7c0686b9
Lynnette Ng <lynnette-ng@outlook.com>
2014-02-25 20:11:43 +0800

    search, undo, list works (manual testing)

---
+++ src/com/taskpad/date/DateManager.java
@@ -0,0 +1,15 @@
+/* Public API to call dates */
+
+package com.taskpad.date;
+
+public class DateManager {
+
+	public static void getTodayDate(){
+		
+	}
+	
+	public static void getTime(){
+		
+	}
+	
+}
