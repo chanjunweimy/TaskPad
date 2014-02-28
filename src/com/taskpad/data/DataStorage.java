4c001fc168d726202a7eabe948c2150362df3537
Wang TaiNing <tainingw@gmail.com>
2014-03-01 02:22:57 +0800

    retrieve function for data storage

--- src/com/taskpad/data/DataStorage.java
+++ src/com/taskpad/data/DataStorage.java
@@ -16,15 +16,54 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import com.taskpad.execute.Task;
 
 public class DataStorage {
-	public static LinkedList<Task> retrieve() {
+	private String file;
+	
+	public LinkedList<Task> retrieve() {
+		LinkedList<Task> listOfTasks = new LinkedList<Task>();
+		
+		try {
+			File fXmlFile = new File(file);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			
+			doc.getDocumentElement().normalize();
+			
+			NodeList nList = doc.getElementsByTagName("task");
+			
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node taskNode = nList.item(i);
+		 
+				if (taskNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element task = (Element) taskNode;
+		 
+					String description = task.getElementsByTagName("description").item(0).getTextContent();
+					String deadline = task.getElementsByTagName("deadline").item(0).getTextContent();
+					String startTime = task.getElementsByTagName("start_time").item(0).getTextContent();
+					String endTime = task.getElementsByTagName("end_time").item(0).getTextContent();
+					String details = task.getElementsByTagName("details").item(0).getTextContent();
+					int done = Integer.parseInt(task.getElementsByTagName("done").item(0).getTextContent());
+					
+					listOfTasks.add(new Task(description, deadline, startTime, endTime, details, done));
+				}
+			}
+			
+			return listOfTasks;
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+		
+		
 		return null;
 	}
 	
-	public static void storeBack(LinkedList<Task> taskList, String file) {
+	public void storeBack(LinkedList<Task> taskList) {
 		try {
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
