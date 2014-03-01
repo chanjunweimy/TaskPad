57c84ae5fb40d4c6db0ae4208894acbef2789662
Wang TaiNing <tainingw@gmail.com>
2014-03-01 16:24:22 +0800

    executor update

--- src/com/taskpad/execute/ExcecutorManager.java
+++ src/com/taskpad/execute/ExcecutorManager.java
@@ -1,18 +1,38 @@
 package com.taskpad.execute;
 
+import java.util.LinkedList;
 import java.util.Map;
 
+import com.taskpad.data.DataManager;
 import com.taskpad.input.Input;
 
 public class ExcecutorManager {
+	private static final String FEEDBACK_ADD = null;
+	
+	private LinkedList<Task> listOfTasks;
 
 	public ExcecutorManager(){
 		
 	}
 	
-	public static void receiveFromInput(Input input){
+	public void receiveFromInput(Input input){
 		String commandType = input.getCommand();
 		Map<String, String> parameters = input.getParameters();
 	}
+
+	private void getListOfTasks(String file) {
+		listOfTasks = DataManager.retrieve(file);
+	}
 	
+	private void add(String description, String deadline,
+			String startTime, String endTime, String details) {
+		Task taskToAdd = new Task(description, deadline, startTime, endTime, details);
+		listOfTasks.add(taskToAdd);
+		passFeedbackToGui(FEEDBACK_ADD);
+	}
+
+	private void passFeedbackToGui() {
+		// TODO Auto-generated method stub
+		
+	}
 }
