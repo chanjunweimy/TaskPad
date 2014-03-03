bbb7948e61034343538c33a78c3eb59ff4ff1ea0
Chan Jun Wei <chanjunweimy@gmail.com>
2014-03-03 13:41:26 +0800

    created CompareDateAndTime class to provide Comparator

---
+++ src/com/taskpad/timeanddate/CompareDateAndTime.java
@@ -0,0 +1,13 @@
+package com.taskpad.timeanddate;
+
+import java.util.Comparator;
+
+//sorting comparator created for executor
+public class CompareDateAndTime {
+	public static final Comparator<String> ACCENDING_ORDER = 
+			new Comparator<String>() {
+		public int compare(String e1, String e2) {
+			return 0;
+		}
+	};
+}
