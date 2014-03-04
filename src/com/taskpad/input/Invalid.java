31672a0d8e37aab34b14cedbe55f5b4d8442cfc5
lynnette <lynnette@localhost>
2014-03-04 17:57:28 +0800

    Search works!

--- src/com/taskpad/input/Invalid.java
+++ src/com/taskpad/input/Invalid.java
@@ -1,5 +1,29 @@
 package com.taskpad.input;
 
-public class Invalid {
+public class Invalid extends Command {
+	
+	private static boolean hasCheckedFlexi = false;
+
+	public Invalid(String input) {
+		super(input);
+	}
+
+	@Override
+	protected boolean commandSpecificRun() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void initialiseParametersToNull() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void putInputParameters() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
