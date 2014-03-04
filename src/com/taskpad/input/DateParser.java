35d75570b1bf2cf3d24b55cfb190d7eaab5ea435
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-04 18:21:27 +0800

    Edited stubs for date time parser

--- src/com/taskpad/input/DateParser.java
+++ src/com/taskpad/input/DateParser.java
@@ -1,9 +1,24 @@
 /**
- * This class is for FlexiCommands for date and time
+ * This class is for FlexiCommands for date
  */
 
 package com.taskpad.input;
 
 public class DateParser {
 
+	/* This method takes in a date and parses it
+	 * 
+	 */
+	public String parseDate(String input){
+		String date = "";
+		return date;
+	}
+	
+	/* This method takes in a date and check if its a valid date - if cannot decode, should throw error to GUI
+	 * 
+	 */
+	public boolean checkIfValidDate(String input){
+		return true;
+	}
+	
 }
--- src/com/taskpad/input/TimeParser.java
+++ src/com/taskpad/input/TimeParser.java
@@ -1,5 +1,25 @@
+/**
+ * This class is for FlexiCommands for time
+ */
+
+
 package com.taskpad.input;
 
 public class TimeParser {
 
+	/* This method takes in a time and parses it
+	 * 
+	 */
+	public String parseTime(String input){
+		String time = "";
+		return time;
+	}
+	
+	/* This method takes in a time and check if its a valid time - if cannot decode, should throw error to GUI
+	 * 
+	 */
+	public boolean checkIfValidTime(String input){
+		return true;
+	}
+	
 }
