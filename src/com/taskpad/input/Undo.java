ebb79d9f0aa0fc6baaa76af31a18fd70b3311160
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-04 16:13:04 +0800

    Undo done

--- src/com/taskpad/input/Undo.java
+++ src/com/taskpad/input/Undo.java
@@ -1,5 +1,38 @@
 package com.taskpad.input;
 
-public class Undo {
+public class Undo extends Command{
+
+	private final static String COMMAND_UNDO = "UNDO";
+	
+	private static String PARAMETER_UNDO = "";
+	
+	public Undo(String input) {
+		super(input);
+		setCOMMAND(COMMAND_UNDO);
+	}
+
+	@Override
+	public void run(){
+		clearInputParameters();
+		initialiseParametersToNull();
+		putInputParameters();
+		createInputObject();
+		passObjectToExecutor();
+	}
+	
+	@Override
+	protected boolean commandSpecificRun() {
+		return true;
+	}
+
+	@Override
+	protected void initialiseParametersToNull() {
+		putOneParameter(PARAMETER_UNDO, "");
+	}
+
+	@Override
+	protected void putInputParameters() {
+		putOneParameter(PARAMETER_UNDO, "");
+	}
 
 }
