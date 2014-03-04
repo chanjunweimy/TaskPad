813b2268839600b362c8cb56497123c35a9b6854
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-04 18:31:02 +0800

    Added more time parser

--- src/com/taskpad/input/TimeParser.java
+++ src/com/taskpad/input/TimeParser.java
@@ -15,11 +15,33 @@ public class TimeParser {
 		return time;
 	}
 	
-	/* This method takes in a time and check if its a valid time - if cannot decode, should throw error to GUI
+	/* This method takes in a time and check if its a valid 24h clock time
+	 * If not, decode it
 	 * 
 	 */
 	public boolean checkIfValidTime(String input){
+		String time = stripTimeDelimiters(input);
+		int timeInt = Integer.parseInt(time);
+		
+		if (timeInt >= 0000 && timeInt <= 2400){
+			return true;
+		} else if (decodeTime(input)){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean decodeTime(String input){
 		return true;
+	}
+	
+	private String stripTimeDelimiters(String input){
+		String time = input.replaceAll(":", "");
+		time = time.replaceAll(" ", "");
+		time = time.replaceAll(".", "");
+		
+		return time;
 	}
 	
 }
