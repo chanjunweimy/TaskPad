1a3ee94e8bf636e3301e4b7ecfc43c26cc880d33
Chan Jun Wei <chanjunweimy@gmail.com>
2014-03-04 16:12:10 +0800

    set comparable to compare task object

--- src/com/taskpad/timeanddate/CompareDateAndTime.java
+++ src/com/taskpad/timeanddate/CompareDateAndTime.java
@@ -2,11 +2,13 @@ package com.taskpad.timeanddate;
 
 import java.util.Comparator;
 
+import com.taskpad.execute.Task;
+
 //sorting comparator created for executor
 public class CompareDateAndTime {
-	public static final Comparator<String> ACCENDING_ORDER = 
-			new Comparator<String>() {
-		public int compare(String e1, String e2) {
+	public static final Comparator<Task> ACCENDING_ORDER = 
+			new Comparator<Task>() {
+		public int compare(Task e1, Task e2) {
 			return 0;
 		}
 	};
