b19f3a5915805f96dd6e5918f3657e7940eaa98b
Chan Jun Wei <chanjunweimy@gmail.com>
2014-03-04 13:45:26 +0800

    dunno what happened to binary file

---
+++ src/com/taskpad/input/InputManager.java
@@ -0,0 +1,50 @@
+/** This class is used for calling APIs from other packages
+ * 
+ * @author Lynnette
+ *
+ */
+
+package com.taskpad.input;
+
+import java.awt.Color;
+
+import com.taskpad.data.DataManager;
+import com.taskpad.execute.ExecutorManager;
+import com.taskpad.ui.GuiManager;
+
+
+public class InputManager {
+	
+	public static void startInputProcessor(){
+		new InputMain();
+	}
+	
+	public static void receiveFromGui(String inputString){
+		InputMain.receiveInput(inputString);
+	}
+
+	public static void outputToGui(String outputString){
+		GuiManager.callOutput(outputString);
+	}
+	
+	public static void outputFormatString(String outputString, Color c, boolean isBold){
+		GuiManager.showSelfDefinedMessage(outputString, c, isBold);
+	}
+	
+	public static void callGuiExit(){
+		GuiManager.callExit();
+	}
+	
+	public static void clearScreen(){
+		GuiManager.clearOutput();
+	}
+	
+	public static void passToExecutor(Input input){
+		ExecutorManager.receiveFromInput(input);
+	}
+	
+	public static int retrieveNumberOfTasks(){
+		return DataManager.retrieveNumberOfTasks();
+	}
+	
+}
