76aeb851a0bfab8c5842b2d785cca049996db389
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-08 08:25:29 +0800

    Fixed the add problem

---
+++ src/com/taskpad/launcher/TaskPadMain.java
@@ -0,0 +1,34 @@
+/**
+ * Author: Chan Jun Wei, Lynnette Ng Hui Xian, Wang Taining
+ * Product: TaskPad
+ * Team: W13-3j
+ */
+
+package com.taskpad.launcher;
+
+import java.util.LinkedList;
+
+import javax.swing.SwingUtilities;
+
+import com.taskpad.data.DataFile;
+import com.taskpad.data.DataManager;
+import com.taskpad.execute.Task;
+
+public class TaskPadMain{	
+
+	private TaskPadMain(){
+	}
+	
+	public static void main(String[] args){
+		runProgram();
+	}
+
+	private static void runProgram() {
+		LinkedList<Task> tasks = new LinkedList<Task>();
+		DataManager.storeBack(tasks, DataFile.FILE);
+		DataManager.storeBack(tasks, DataFile.FILE_PREV);
+		
+		Runnable runTaskPad = new TaskPadLauncher();
+		SwingUtilities.invokeLater(runTaskPad);
+	}
+}
