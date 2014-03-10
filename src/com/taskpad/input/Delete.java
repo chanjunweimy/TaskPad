8114901f9021ae5e3efdd3bb44b5124fb20b56d7
Wang TaiNing <tainingw@gmail.com>
2014-03-10 19:57:52 +0800

    output for addinfo

---
+++ src/com/taskpad/input/Delete.java
@@ -0,0 +1,42 @@
+/**
+ * This class creates a Delete object
+ * 
+ * Current syntax for delete: del <taskID>
+ * 
+ * Returns Input object
+ * 
+ */
+
+package com.taskpad.input;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Delete extends Command{
+	
+	private static String COMMAND_DELETE = "DELETE";
+	private static String PARAMETER_TASK_ID = "TASKID";
+	private static int NUMBER_ARGUMENTS = 1;		//Number of arguments for delete
+
+	public Delete(String input, String fullInput) {
+		super(input, fullInput);
+		setNUMBER_ARGUMENTS(NUMBER_ARGUMENTS);
+		setCOMMAND(COMMAND_DELETE);
+	}
+
+	@Override
+	protected boolean commandSpecificRun() {
+		return true;
+	}
+
+	@Override
+	protected void initialiseParametersToNull() {
+		inputParameters.put(PARAMETER_TASK_ID, "");		
+	}
+
+	@Override
+	protected void putInputParameters() {
+		putOneParameter(PARAMETER_TASK_ID, input);
+	}
+	
+}
---
+++ src/com/taskpad/input/Search.java
@@ -0,0 +1,48 @@
+package com.taskpad.input;
+
+public class Search extends Command{
+	
+	private static final String COMMAND_SEARCH = "SEARCH";
+	private static final int NUMBER_ARGUMENTS = 1;
+	
+	private static String PARAMETER_KEYWORD = "KEY";
+	private static String _keyword;
+
+	public Search(String input, String fullInput) {
+		super(input, fullInput);
+		setNUMBER_ARGUMENTS(NUMBER_ARGUMENTS);
+		setCOMMAND(COMMAND_SEARCH);
+	}
+
+	@Override
+	public void run(){
+		if (checkIfEmptyString()){
+			return; 
+		}
+		initialiseParametersToNull();
+		putInputParameters();
+		createInputObject();
+	}
+	
+	@Override
+	protected boolean commandSpecificRun() {
+		_keyword = input;
+		return true;
+	}
+
+	@Override
+	protected void initialiseParametersToNull() {
+		putOneParameter(PARAMETER_KEYWORD, "");
+	}
+
+	@Override
+	protected void putInputParameters() {
+		putOneParameter(PARAMETER_KEYWORD, _keyword);
+	}
+	
+	@Override
+	protected boolean checkIfIncorrectArguments(){
+		return false;
+	}
+
+}
