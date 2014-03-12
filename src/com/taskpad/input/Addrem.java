59d851eaa05723c8eae96440b11532b829955331
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-12 13:21:57 +0800

    YAY fixed addrem!

--- src/com/taskpad/input/Addrem.java
+++ src/com/taskpad/input/Addrem.java
@@ -53,11 +53,14 @@ public class Addrem extends Command{
 	
 	@Override
 	protected boolean isNotNumberArgs(String[] inputString){
-		if (inputString.length != getNUMBER_ARGUMENTS() ||
-				inputString.length != getNUMBER_ARGUMENTS()+1	){
-			return true;
+		System.out.println(inputString.length);
+		if (inputString.length == getNUMBER_ARGUMENTS() ||
+				inputString.length == getNUMBER_ARGUMENTS()+1 || 
+				inputString.length == getNUMBER_ARGUMENTS()+2 || 
+				inputString.length == getNUMBER_ARGUMENTS()+3){
+			return false;
 		}
-		return false;
+		return true;
 	}
 	
 	@SuppressWarnings("resource")
