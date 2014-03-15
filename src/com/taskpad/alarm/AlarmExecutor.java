53a61ea249e3979bdc19a1c5f98cf9956a995de7
Wang TaiNing <tainingw@gmail.com>
2014-03-15 23:22:09 +0800

    fixed database problem

---
+++ src/com/taskpad/alarm/AlarmExecutor.java
@@ -0,0 +1,48 @@
+package com.taskpad.alarm;
+
+import com.taskpad.ui.GuiManager;
+
+
+/**
+ * 
+ * @author Jun
+ *
+ * temp class that is currently not put
+ * into executor. It should be implemented inside
+ * executor anyway
+ * 
+ * note that: now Alarm is without desrcription,
+ * we should have description in later stage
+ * 
+ * and note that: Alarm can only support 
+ * 10 s, not 10s
+ * 
+ */
+public class AlarmExecutor {
+	
+	private static final String ERROR_NEGATIVE_DELAY = "Time should be a positive integer";
+	private static final int TIME_FORCE_WAIT = 1;
+	private static TimerObject _forceWaitTimer = new TimerObject();
+	
+	private static String _desc = "";
+	
+	private AlarmExecutor(int time){
+	}
+	
+	protected static void initializeAlarm(String desc, int time){
+		_desc = desc;
+		if (time >= 1){
+			_forceWaitTimer.setForceStopTimer(TIME_FORCE_WAIT, time - TIME_FORCE_WAIT);
+		} else {
+			GuiManager.callOutput(ERROR_NEGATIVE_DELAY);
+		}
+	}
+	
+	protected static void launchAlarm(int time) {//it should be a method in executor		
+		try {
+			AlarmManager.setAlarm(_desc, time);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
---
+++ src/com/taskpad/ui/GuiFrame.java
@@ -0,0 +1,238 @@
+package com.taskpad.ui;
+
+import java.awt.Frame;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.NativeInputEvent;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public abstract class GuiFrame extends JFrame implements NativeKeyListener, WindowListener{
+
+	private static final long serialVersionUID = 1L;
+	
+	protected final double COMPUTER_WIDTH = 
+			Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+	protected final double COMPUTER_HEIGHT = 
+			Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+	
+	protected GuiFrame(){
+		initalizeGuiFrame();
+	}
+
+	private void initalizeGuiFrame() {
+		//to disable the titlebar
+		setUndecorated(true);
+		
+		addWindowListener(this);
+		
+		showWindow(true);
+		
+		focusInputBox();
+		
+		//to clear the memory
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void focusInputBox() {
+		//to make it focus to input box
+		minimizeOrRestore();
+		minimizeOrRestore();
+	}
+	
+	protected void close(){
+		dispose();
+	}
+	
+	protected void showWindow(boolean isVisible){
+		setVisible(isVisible);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		//Initialize native hook.
+        try {
+                GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+                System.err.println("There was a problem registering the native hook.");
+                System.err.println(ex.getMessage());
+                ex.printStackTrace();
+
+                System.exit(1);
+        }
+
+        GlobalScreen.getInstance().addNativeKeyListener(this);
+        requestFocusInWindow();
+	}
+	
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		 endProgram();
+	}
+	
+	@Override
+	public void nativeKeyPressed(NativeKeyEvent arg0) {
+		
+		boolean isEscapeKey = arg0.getKeyCode() == NativeKeyEvent.VK_ESCAPE;
+		boolean isShiftSpaceKey = arg0.getKeyCode() == NativeKeyEvent.VK_SPACE 
+				&& NativeInputEvent.getModifiersText(arg0.getModifiers()).
+				equals("Shift");
+		boolean isAltEndKey = arg0.getKeyCode() == NativeKeyEvent.VK_END 
+				&& NativeInputEvent.getModifiersText(arg0.getModifiers()).
+				equals("Alt");
+		boolean isAltAKey = arg0.getKeyCode() == NativeKeyEvent.VK_A 
+				&& NativeInputEvent.getModifiersText(arg0.getModifiers()).
+				equals("Alt");
+		boolean isAltCKey = arg0.getKeyCode() == NativeKeyEvent.VK_C
+				&& NativeInputEvent.getModifiersText(arg0.getModifiers()).
+				equals("Alt");
+		
+		/**
+		 * @author Jun
+		 * we will disable some keys when TaskPad is in
+		 * hiding mode
+		 */
+		isEscapeKey = isEscapeKey && isVisible();
+		isAltEndKey = isAltEndKey && isVisible();
+		isAltCKey = isAltCKey && isVisible();
+		
+		if (isAltEndKey) {
+			minimizeOrRestore();
+		} else if (isShiftSpaceKey){
+			hideOrShow();
+		} else if (isEscapeKey){
+			endProgram();
+		} else if (isAltAKey){
+			switchOffAlarm();
+		} else if (isAltCKey){
+			cancelAlarms();
+		}
+	}
+
+	private void cancelAlarms() {
+		try {
+			GuiManager.cancelAlarms();
+		} catch (Exception e) {
+			//do nothing
+		}
+	}
+
+	private void switchOffAlarm() {
+		try {
+			GuiManager.turnOffAlarm();
+		} catch (Exception e) {
+			//System.err.println(e.getMessage());
+			//do nothing
+		}
+	}
+
+	private void hideOrShow() {
+		Runnable changeVisibility = getVisibilityChanges();
+		SwingUtilities.invokeLater(changeVisibility);
+	}
+
+	private void minimizeOrRestore() {
+		Runnable changeState = getStateChanges();
+		SwingUtilities.invokeLater(changeState);
+	}
+
+	private void endProgram() {
+		//Clean up the native hook.
+        GlobalScreen.unregisterNativeHook();
+        System.runFinalization();
+        System.exit(0);
+	}
+	
+	private Runnable getVisibilityChanges() {
+		Runnable changeVisibility = new Runnable(){
+			public void run(){
+				boolean isShown = isVisible() == true;
+				boolean isHided = isVisible() == false;
+				if (isShown){
+				  	hide();
+				} else if (isHided){
+					show();
+				}
+				focusInputBox();
+			}
+ 
+			private void show() {
+				showWindow(true);
+				setState(Frame.NORMAL);
+			}
+
+			private void hide() {
+				showWindow(false);
+			}
+		};
+		return changeVisibility;
+	}
+
+	private Runnable getStateChanges() {
+		Runnable changeState = new Runnable(){
+			public void run(){
+				boolean isMinimized = getExtendedState() == Frame.ICONIFIED;
+				boolean isRestored = getExtendedState() == Frame.NORMAL;
+				if (isMinimized){
+					restore();
+				} else if (isRestored){
+					minimize();
+				}
+			}
+
+			private void minimize() {
+				setState(Frame.ICONIFIED);
+			}
+
+			private void restore() {
+				setState(Frame.NORMAL);
+			}
+		};
+		return changeState;
+	}
+	
+	
+	//won't implement
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void nativeKeyReleased(NativeKeyEvent arg0) {
+		
+	}
+
+	@Override
+	public void nativeKeyTyped(NativeKeyEvent arg0) {
+		
+	}
+}
