5a702a145fa251ac3f5a2353c2e5437a1b1fc7d5
Wang TaiNing <tainingw@gmail.com>
2014-03-16 00:14:45 +0800

    multiple redo implemented

--- src/com/taskpad/data/CommandRecord.java
+++ src/com/taskpad/data/CommandRecord.java
@@ -4,17 +4,30 @@ import java.util.LinkedList;
 
 public class CommandRecord {
 	// private static String previousCommand;
-	private static LinkedList<String> commands = new LinkedList<String>();
+	private static LinkedList<String> commandsForUndo = new LinkedList<String>();
+	private static LinkedList<String> commandsForRedo = new LinkedList<String>();
 	
-	public static void push(String command) {
+	public static void pushForUndo(String command) {
 		// previousCommand = command;
-		commands.push(command);
+		commandsForUndo.push(command);
 	}
 	
-	public static String pop() throws NoPreviousCommandException {
-		if (commands.size() < 1) {
+	public static String popForUndo() throws NoPreviousCommandException {
+		if (commandsForUndo.size() < 1) {
 			throw new NoPreviousCommandException();
 		}
-		return commands.pop();
+		return commandsForUndo.pop();
+	}
+	
+	public static void pushForRedo(String command) {
+		// previousCommand = command;
+		commandsForRedo.push(command);
+	}
+	
+	public static String popForRedo() throws NoPreviousCommandException {
+		if (commandsForRedo.size() < 1) {
+			throw new NoPreviousCommandException();
+		}
+		return commandsForRedo.pop();
 	}
 }
