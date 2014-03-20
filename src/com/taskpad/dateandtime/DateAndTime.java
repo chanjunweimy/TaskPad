da84f257cd035d8b478a3499854866da040e5395
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-20 18:31:05 +0800

    Commit assert

---
+++ src/com/taskpad/dateandtime/DateAndTime.java
@@ -0,0 +1,36 @@
+/* This helper class creates the date and time object */
+
+package com.taskpad.dateandtime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateAndTime {	
+	private SimpleDateFormat _formater;
+	private Date _today;
+	
+	protected DateAndTime(){
+		_today = new Date();
+	}
+	
+	protected String getCurrentDate(){
+		_formater = new SimpleDateFormat("dd/MM/yyyy");
+		return _formater.format(_today);
+	}
+	
+	protected String getCurrentTime(){
+		_formater = new SimpleDateFormat("HH:mm");
+		return _formater.format(_today);
+	}
+	
+	protected String getCurrentDay(){
+		_formater = new SimpleDateFormat("E");
+		return _formater.format(_today);
+	}
+	
+	protected String getCurrentTimeAndDate(){
+		_formater = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		return _formater.format(_today);
+	}
+	
+}
---
+++ src/com/taskpad/dateandtime/DateAndTimeManager.java
@@ -0,0 +1,162 @@
+/* Public API to call dates */
+
+package com.taskpad.dateandtime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+
+import com.taskpad.execute.Task;
+import com.taskpad.ui.GuiManager;
+
+
+/**
+ * 
+ * @author Jun
+ *
+ * use singleton to 
+ * implement this class
+ * because a manager can do all the parsing
+ * and can return all the date and time
+ *
+ */
+public class DateAndTimeManager implements TimeSkeleton, DateSkeleton {
+	private static final String SPACE = " ";
+	private final static String ERROR = "ERROR!";
+	private final static int CONVERT = 60;
+	private static final Exception EXCEPTION_INVALID_INPUT = new Exception();//don't know choose which
+	private static final String EMPTY = "";
+
+	
+	private static int _multiple = 1;
+	private static DateAndTime _dateAndTimeObject = null;
+	
+	private static DateAndTimeManager _managerInstance = new DateAndTimeManager();
+	
+	private DateAndTimeManager(){
+	}
+
+	@Override
+	public String getTodayTime(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentTime();
+	}
+	
+	@Override
+	public String getTodayDate(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentDate();
+	}
+	
+	@Override
+	public String getTodayDay(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentDay();
+	}
+	
+	public String getTodayDateAndTime(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentTimeAndDate();
+	}
+
+	public static final Comparator<Task> ACCENDING_ORDER = 
+			new Comparator<Task>() {
+			@Override
+		public int compare(Task e1, Task e2) {
+			SimpleDateFormat dateConverter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			Date d1, d2;
+			try {
+				d1 = dateConverter.parse(e1.getDeadline() + e1.getEndTime());
+				d2 = dateConverter.parse(e2.getDeadline() + e2.getEndTime());
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				System.err.println(e.getMessage());
+				return 0;
+			}
+			return d1.compareTo(d2);
+		}
+	};
+	
+	
+	/**
+	 * These 2 methods are going to implement
+	 * to do the parseDate() and parseTime() thing
+	 * @return
+	 */
+	public String parseDate(String dateString){
+		return null;
+	}
+	
+	public String parseTime(String timeString) throws Exception{
+		String inputString[] = timeString.split(SPACE);
+		int length = inputString.length;
+		
+		String numberString = inputString[length-2].trim();
+		numberString = parseNumber(numberString);
+		
+		int t = Integer.parseInt(numberString);
+		
+		String unit = inputString[length - 1];
+		calculateMultiple(unit);
+		
+		t *= _multiple;
+		
+		return t + EMPTY;
+	}
+	
+	public String parseNumber(String numberString) throws NullPointerException{
+		NumberParser parser = new NumberParser();
+		return parser.parseTheNumbers(numberString);
+	}
+	
+	private void calculateMultiple(String unit) throws Exception {
+		switch (unit.toLowerCase()){
+		case "s":
+		case "second":
+		case "seconds":
+		case "sec":
+		case "secs":
+			setMultiple(_multiple);
+			break;
+
+		case "m":
+		case "minute":
+		case "minutes":
+		case "min":
+		case "mins":
+			setMultiple(_multiple * CONVERT);
+			break;
+
+		case "h":
+		case "hour":
+		case "hours":
+		case "hr":
+		case "hrs":
+			setMultiple(_multiple * CONVERT * CONVERT);
+			break;
+
+		default:
+			GuiManager.callOutput(ERROR);
+			throw EXCEPTION_INVALID_INPUT;
+		}
+	}
+	
+	private void setMultiple(int multiple) {
+		_multiple = multiple;
+	}
+
+	public static DateAndTimeManager getInstance() {
+		return _managerInstance;
+	}
+	
+	//for debug:
+	/*
+	public static void main(String[] args){
+		DateAndTimeManager now = new DateAndTimeManager();
+		System.out.println(now.getTodayTime());
+		System.out.println(now.getTodayDate());
+		System.out.println(now.getTodayDay());
+	}
+	*/
+}
