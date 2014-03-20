afc7316f125fd9ec6c8165dcf5b113ae43182d9a
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-20 20:11:19 +0800

    Finished up some exceptions for input

--- src/com/taskpad/input/TaskIDException.java
+++ src/com/taskpad/input/TaskIDException.java
@@ -2,11 +2,8 @@ package com.taskpad.input;
 
 public class TaskIDException extends Exception {
 	
-	protected static final String MESSAGE_INVALID_INPUT = "Error: Invalid input: %s";
-
 	public TaskIDException(String message){
-		String errorMessage = String.format(MESSAGE_INVALID_INPUT, message);
-		InputManager.outputToGui(errorMessage);
+		
 	}
 	
 }
