752775d965ff8ebdfaf757dc0605de2c5d9938da
Chan Jun Wei <chanjunweimy@gmail.com>
2014-03-24 02:43:45 +0800

    Found the reason why DateAndTimeManager can't use to create log file.
    It is because of the sign "/"
    This piece of code can be refactored in a much more
    better way. :)

---
+++ src/com/taskpad/dateandtime/DateAndTimeManager.java
@@ -0,0 +1,230 @@
+/* Public API to call dates */
+
+package com.taskpad.dateandtime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+
+import com.taskpad.storage.Task;
+
+
+
+/**
+ * 
+ * @author Jun & Lynnette
+ *
+ * DateAndTimeManager is a facade class of dateandtime package.
+ * 
+ * We use singleton to 
+ * implement this class
+ * because a manager can do all the parsing
+ * and can return all the date and time
+ *
+ */
+public class DateAndTimeManager implements TimeSkeleton, DateSkeleton {
+	/* DEPRECATED
+	private static final String SPACE = " ";
+	private final static String ERROR = "ERROR!";
+	private final static int CONVERT = 60;
+	private static final Exception EXCEPTION_INVALID_INPUT = new Exception();//don't know choose which
+	private static final String EMPTY = "";
+
+	
+	private static int _multiple = 1;
+	*/
+	
+	private static DateAndTime _dateAndTimeObject = null;
+	
+	private static DateAndTimeManager _managerInstance = new DateAndTimeManager();
+	
+	private DateAndTimeManager(){
+	}
+	
+	/**
+	 * getInstance: get the instance of DateAndTimeManager to use dateandtime api..
+	 * @return DateAndTimeManager
+	 */
+	public static DateAndTimeManager getInstance() {
+		return _managerInstance;
+	}
+
+	/**
+	 * getTodayTime: return the currect time.
+	 * @return String
+	 */
+	@Override
+	public String getTodayTime(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentTime();
+	}
+	
+	/**
+	 * getTodayDate: return today's date
+	 * @return String
+	 */
+	@Override
+	public String getTodayDate(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentDate();
+	}
+	
+	/**
+	 * getTodayDay: return today's day
+	 * @return String
+	 */
+	@Override
+	public String getTodayDay(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentDay();
+	}
+	
+	/**
+	 * getTodayDateAndTime: to get today's date and the current time
+	 * @return String
+	 */
+	public String getTodayDateAndTime(){
+		_dateAndTimeObject = new DateAndTime();
+		return _dateAndTimeObject.getCurrentTimeAndDate();
+	}
+
+	/**
+	 * ACCENDING_ORDER: a comparator used to sort Task by their dates.
+	 */
+	public static final Comparator<Task> ACCENDING_ORDER = 
+			new Comparator<Task>() {
+		/**
+		 * compare: compare two tasks' Date
+		 * @param e1: task1
+		 * @param e2: task2
+		 * @return int
+		 */
+			@Override
+		public int compare(Task e1, Task e2) {
+			SimpleDateFormat dateConverter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			Date d1, d2;
+			try {
+				d1 = dateConverter.parse(e1.getDeadline() + e1.getEndTime());
+				d2 = dateConverter.parse(e2.getDeadline() + e2.getEndTime());
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				System.err.println(e.getMessage());
+				return 0;
+			}
+			return d1.compareTo(d2);
+		}
+	};
+	
+	
+	/**
+	 * These 2 methods are going to implement
+	 * to do the parseDate() and parseTime() thing
+	 * @return timeString, nicely formatted
+	 */
+	public String parseDate(String dateString){
+		return null;
+	}
+	
+	public String parseTime(String timeString) throws NullTimeUnitException, NullTimeValueException, TimeErrorException{
+		return TimeParser.parseTime(timeString);
+	}
+	
+	public String parseTimeInput(String timeString) throws TimeErrorException{
+		return TimeParser.parseTimeInput(timeString);
+	}
+	
+	/**
+	 * convertToSecond: convert time from any unit to second
+	 * @param timeString: time value + time unit, ex: 1 min, one min, 1s
+	 * @return: String
+	 * @throws NullTimeUnitException: User did not key in time unit
+	 * @throws NullTimeValueException: User did not key in time value / not valid time value
+	 */
+	public String convertToSecond(String timeString) throws NullTimeUnitException, NullTimeValueException{
+		TimeWordParser twp = TimeWordParser.getInstance();
+		return twp.parseTimeWord(timeString);
+	}
+	
+	/**
+	 * parseNumber: parse a languange number to a real number String, ex: one to 1
+	 * @param numberString: language number or normal number
+	 * @return String
+	 */
+	public String parseNumber(String numberString){
+		NumberParser parser = NumberParser.getInstance();
+		return parser.parseTheNumbers(numberString);
+	}
+	
+	
+	/* DEPRECATED
+	public String parseTime(String timeString) throws Exception{
+		String inputString[] = timeString.split(SPACE);
+		int length = inputString.length;
+		
+		String numberString = inputString[length-2].trim();
+		numberString = parseNumber(numberString);
+		
+		int t = Integer.parseInt(numberString);
+		
+		String unit = inputString[length - 1];
+		calculateMultiple(unit);
+		
+		t *= _multiple;
+		
+		return t + EMPTY;
+	}
+	 */
+	
+	
+	
+	/* DEPRECATED
+	private void calculateMultiple(String unit) throws Exception {
+		switch (unit.toLowerCase()){
+		case "s":
+		case "second":
+		case "seconds":
+		case "sec":
+		case "secs":
+			setMultiple(_multiple);
+			break;
+
+		case "m":
+		case "minute":
+		case "minutes":
+		case "min":
+		case "mins":
+			setMultiple(_multiple * CONVERT);
+			break;
+
+		case "h":
+		case "hour":
+		case "hours":
+		case "hr":
+		case "hrs":
+			setMultiple(_multiple * CONVERT * CONVERT);
+			break;
+
+		default:
+			GuiManager.callOutput(ERROR);
+			throw EXCEPTION_INVALID_INPUT;
+		}
+	}
+	
+	private void setMultiple(int multiple) {
+		_multiple = multiple;
+	}
+	*/
+
+	
+	
+	//for debug:
+	/*
+	public static void main(String[] args){
+		DateAndTimeManager now = new DateAndTimeManager();
+		System.out.println(now.getTodayTime());
+		System.out.println(now.getTodayDate());
+		System.out.println(now.getTodayDay());
+	}
+	*/
+}
---
+++ src/com/taskpad/input/InputMain.java
@@ -0,0 +1,349 @@
+package com.taskpad.input;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import com.taskpad.alarm.AlarmManager;
+import com.taskpad.input.CommandTypes.CommandType;
+
+public class InputMain {
+
+	private static final String MESSAGE_CONFIRMATION_CLEAR_SCREEN = "Confirm clear screen? (Y/N)";
+	private static final String MESSAGE_CONFIRMATION_CLEAR_DATA = "Confirm clear data? (Y/N)";
+	private static final String MESSAGE_NOT_CLEAR_DATA = "Not clearing data";
+	private static final String MESSAGE_NOT_CLEAR_SCREEN = "Not clearing Screen";
+	private static final String MESSAGE_INVALID_COMMAND = "Invalid Command: %s ";
+	
+	private static final String COMMAND_CLEAR = "CLEAR";
+	private static final String COMMAND_CLEAR_SCREEN = "CLEARSCREEN";
+	
+	private static final String STRING_EMPTY = "";
+			
+	@SuppressWarnings("unused")
+	private static CommandTypes commandTypes = new CommandTypes();
+	@SuppressWarnings("unused")
+	private static Input inputObject;
+	private static boolean isConfirmation = false;
+	protected static boolean hasCheckedFlexi = false;
+	private static String currentCommand = STRING_EMPTY;
+	
+	@SuppressWarnings("unused")
+	private static Map<String, String> inputParameters = new HashMap<String, String>();
+	
+	private static Logger logger = Logger.getLogger("TaskPad");
+	
+	protected static String receiveInput(String input){
+		input = input.trim();
+		String outputString = STRING_EMPTY;
+		hasCheckedFlexi = false;
+		
+		if (isConfirmation){
+			processConfirmation(input);
+		} else {
+			String inputCopy = input;
+			if (errorIfNoInput(input)){
+				ErrorMessages.emptyInputMessage();
+			}
+				
+			String commandTypeString = parseInput(inputCopy);
+			CommandTypes.CommandType commandType = determineCommandType(commandTypeString);
+			logger.info("Command: " + commandType.toString());
+			
+			if (isValidCommandType(commandType)){		
+				commandTypeString = removeFirstWord(input);
+				outputString = commandType.toString() + " " + commandTypeString;
+				performCommand (commandType, commandTypeString, input);
+			} else if (hasCheckedFlexi){
+				invalidCommand(input);
+				outputString += commandType.toString();
+			} else {
+				hasCheckedFlexi = true;
+				outputString = flexiCommand(input);
+			}
+		}
+		return outputString;
+	}
+	
+	private static void processConfirmation(String input) {
+		if (isConfirmClear(input)){
+			executeConfirmation();
+		} else if (isNotClearing(input)){
+			processNoClear();
+		} else {
+			ErrorMessages.invalidConfirmationInput();
+		}
+		resetConfirmationVariable();		
+	}
+
+	private static boolean isConfirmClear(String input){
+		return input.equalsIgnoreCase("Y");
+	}
+	
+	private static boolean isNotClearing(String input){
+		return input.equalsIgnoreCase("N");
+	}
+	
+	private static void executeConfirmation(){
+		if (currentCommand.equals(COMMAND_CLEAR)){
+			clearAllTasks();
+		} else if (currentCommand.equals(COMMAND_CLEAR_SCREEN)){
+			InputManager.clearScreen();
+		}
+	}
+	
+	private static void processNoClear(){
+		if (currentCommand.equals(COMMAND_CLEAR)){
+			InputManager.outputToGui(MESSAGE_NOT_CLEAR_DATA);
+		} else if (currentCommand.equals(COMMAND_CLEAR_SCREEN)){
+			InputManager.outputToGui(MESSAGE_NOT_CLEAR_SCREEN);
+		}
+	}
+	
+	private static void resetConfirmationVariable(){
+		setConfirmationFalse();
+		setCurrCommandEmpty();
+	}
+
+	private static void setCurrCommandEmpty() {
+		currentCommand = STRING_EMPTY;
+	}
+
+	private static void setConfirmationFalse() {
+		isConfirmation = false;
+	}
+	
+	private static boolean errorIfNoInput(String input) {
+		return input.equals(STRING_EMPTY);
+	}
+
+	@SuppressWarnings("static-access")
+	private static boolean isValidCommandType(CommandType commandType) {
+		return !commandType.equals(commandType.INVALID);
+	}
+
+	private static void performCommand(CommandType commandType, String commandTypeString, String input) {
+		switch(commandType){
+			case ADD:
+				addTask(commandTypeString, input);
+				break;
+			case ALARM:
+				setUpAlarm(commandTypeString, input);
+				break;
+			case ADD_INFO:
+				addInfoTask(commandTypeString, input);
+				break;
+			case ADD_REM:
+				addRemTask(commandTypeString, input);
+				break;
+			case ADD_PRI:
+				addPriTask(commandTypeString, input);
+				break;
+			case LIST:
+				listTask(commandTypeString, input);
+				break;
+			case CLEAR_ALL:
+				isConfirmation = true;
+				currentCommand = COMMAND_CLEAR;
+				clearAllTasksConfirmation();
+				break;
+			case CLEAR_SCREEN:
+				isConfirmation = true;
+				currentCommand = COMMAND_CLEAR_SCREEN;
+				clearScreen();
+				break;
+			case DELETE:
+				deleteTask(commandTypeString, input);
+				break;
+			case DONE:
+				doneTask(commandTypeString, input);
+				break;
+			case EDIT:
+				editTask(commandTypeString, input);
+				break;
+			case SEARCH:
+				searchTask(commandTypeString, input);
+				break;
+			case STOP:
+				stopAlarm(commandTypeString, input);
+				break;
+			case HELP:
+				help();
+				break;
+			case EXIT:
+				exitProgram();
+				break;
+			case REDO:
+				redoTask();
+				break;
+			case UNDO:
+				undoLast();
+				break;
+			default:
+				invalidCommand(commandTypeString);
+				break;
+		}
+	}
+
+	/* Methods to perform commands */
+
+	private static void addTask(String input, String fullInput) {
+		Add add = new Add(input, fullInput);
+		add.run();
+	}
+	
+	private static void addPriTask(String input, String fullInput){
+		AddPri addPri = new AddPri(input, fullInput);
+		addPri.run();
+	}
+
+	private static void addInfoTask(String input, String fullInput) {
+		Addinfo addinfo = new Addinfo(input, fullInput);
+		addinfo.run();
+	}
+	
+	private static void addRemTask(String input, String fullInput){
+		Addrem addRem = new Addrem(input, fullInput);
+		addRem.run();
+	}
+	
+	private static void listTask(String input, String fullInput){
+		List list = new List(input, fullInput);
+		list.run();
+	}
+	
+	private static void redoTask(){
+		Redo redo = new Redo(STRING_EMPTY, "REDO");
+		redo.run();
+	}
+	
+	private static void deleteTask(String input, String fullInput) {
+		Delete delete = new Delete(input, fullInput);
+		delete.run();
+	}
+
+	private static void doneTask(String input, String fullInput) {
+		Done done = new Done(input, fullInput);
+		done.run();
+	}
+
+	private static void clearAllTasksConfirmation(){
+		InputManager.outputToGui(MESSAGE_CONFIRMATION_CLEAR_DATA);
+	}
+	
+	private static void clearAllTasks() {
+		ClearTasks clearTask = new ClearTasks(STRING_EMPTY, "CLEAR");
+		clearTask.run();
+	}
+	
+	private static void clearScreen(){
+		InputManager.outputToGui(MESSAGE_CONFIRMATION_CLEAR_SCREEN);
+	}
+	
+	private static void undoLast() {
+		Undo undo = new Undo(STRING_EMPTY, "UNDO");
+		undo.run();
+	}
+	
+	private static void editTask(String input, String fullInput) {
+		Edit edit = new Edit(input, fullInput);
+		edit.run();
+	}
+
+	private static void searchTask(String input, String fullInput) {
+		Search search = new Search(input, fullInput);
+		search.run();	
+	}
+	
+	private static void stopAlarm(String input, String fullInput){
+		try {
+			AlarmManager.turnOffAlarm();
+		} catch (Exception e) {
+			return;
+		}
+	}
+	
+	private static void help() {
+		Help help = new Help();
+		help.outputHelp();
+	}
+
+	private static void exitProgram() {
+		InputManager.callGuiExit();
+	}
+	
+	private static void setUpAlarm(String input, String fullInput) {
+		new Alarm(input, fullInput);
+	}
+	
+	private static String flexiCommand(String input){
+		hasCheckedFlexi = true;
+		CommandType command = CommandQueue.findFlexi(input);
+		logger.info("Flexicommands: " + command.toString());
+		String commandTypeString = replaceCommandWord(input, command);
+		performCommand(command, commandTypeString, input);
+		
+		return command.toString() + " " + commandTypeString;
+	}
+	
+	private static String replaceCommandWord (String input, CommandType command){
+		//return input.replaceFirst("(?i)"+command.toString()+" ", "");  //deprecated
+		String desc = STRING_EMPTY;
+		String[] splitInput = input.split(" ");
+		
+		for (int i=0; i<splitInput.length; i++){
+			if (isNotCommandString(command, splitInput, i)){
+				desc += splitInput[i] + " ";
+			}
+		}
+			
+		return desc;
+	}
+
+	private static boolean isNotCommandString(CommandType command,
+			String[] splitInput, int i) {
+		return !splitInput[i].toUpperCase().equals(command.toString());
+	}
+	
+	private static void invalidCommand(String input) {
+		InputManager.outputToGui(String.format(MESSAGE_INVALID_COMMAND, input));	
+	}
+
+	private static CommandTypes.CommandType determineCommandType(String commandTypeString) {
+		String commandToFind = getFirstWord(commandTypeString);
+		CommandTypes.CommandType commandType = CommandQueue.find(commandToFind);
+		
+		return commandType;
+	}
+
+	private static String parseInput(String input) {
+		if (isInvalidCommand(input)){
+			return invalidInput(input);
+		}
+		
+		String commandTypeString = getFirstWord(input);
+		return commandTypeString;			
+	}
+	
+	/* Helper methods for parsing commands */ 
+	private static boolean isInvalidCommand(String userCommand) {
+		if (userCommand.trim().equals(STRING_EMPTY)){
+			return true;
+		}
+		return false;
+	}
+	
+	private static String invalidInput(String input) {
+		return String.format(MESSAGE_INVALID_COMMAND, input);
+	}
+	
+	private static String removeFirstWord(String input) {
+		return input.replaceFirst(getFirstWord(input), STRING_EMPTY).trim();
+	}
+	
+	private static String getFirstWord(String input) {
+		String commandTypeString = input.trim().split("\\s+")[0];
+		return commandTypeString;
+	}
+
+}
