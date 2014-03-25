2839f5ebe368f34cf8aca500ab8ae4d22faf386c
Chan Jun Wei <chanjunweimy@gmail.com>
2014-03-26 07:05:03 +0800

    integrate Add with SimpleDateParser

--- src/com/taskpad/tests/TestDateParser.java
+++ src/com/taskpad/tests/TestDateParser.java
@@ -12,152 +12,277 @@ public class TestDateParser {
 	
 	@Test
 	public void test1() {
-		testDateCommand("11/11/94", "11/11/1994");
+		testDateCommand("11/11/14", "11/11/2014");
 	}
 
 	@Test
 	public void test2() {
-		testDateCommand("11-11-08", "11/11/2008");
+		testDateCommand("11-11-18", "11/11/2018");
 	}
 	
 	@Test
 	public void test3() {
-		testDateCommand("11.11.2000", "11/11/2000");
+		testDateCommand("11.11.2015", "11/11/2015");
 	}
 	
 	@Test
 	public void test4(){
-		testDateCommand("18 10 93", "18/10/1993");
+		testDateCommand("18 10 15", "18/10/2015");
 	}
 	
 	@Test 
 	public void test5(){
-		testDateCommand("Oct 18 93", "18/10/1993");
+		testDateCommand("Oct 18 15", "18/10/2015");
 	}
 	
 	@Test
 	public void test6(){
-		testDateCommand("October 18 93", "18/10/1993");
+		testDateCommand("October 18 14", "18/10/2014");
 	}
 	
 	@Test
 	public void test7(){
-		testDateCommand("Oct 18,93", "18/10/1993");
+		testDateCommand("Oct 18,16", "18/10/2016");
 	}
 	
 	@Test
 	public void test8(){
-		testDateCommand("October 18,93", "18/10/1993");
+		testDateCommand("October 18,14", "18/10/2014");
 	}
 	
 	@Test
 	public void test9(){
-		testDateCommand("Oct 18 , 93", "18/10/1993");
+		testDateCommand("Oct 18 , 14", "18/10/2014");
 	}
 	
 	@Test
 	public void test10(){
-		testDateCommand("October 18 , 93", "18/10/1993");
+		testDateCommand("October 18 , 15", "18/10/2015");
 	}
 	
 	@Test
 	public void test11(){
-		testDateCommand("Oct 18, 93", "18/10/1993");
+		testDateCommand("Oct 18, 14", "18/10/2014");
 	}
 	
 	@Test
 	public void test12(){
-		testDateCommand("October 18, 93", "18/10/1993");
+		testDateCommand("October 18, 14", "18/10/2014");
 	}
 	
 	@Test
 	public void test13(){
-		testDateCommand("18 Oct 93", "18/10/1993");
+		testDateCommand("18 Oct 16", "18/10/2016");
 	}
 	
 	@Test
 	public void test14(){
-		testDateCommand("18 October 93", "18/10/1993");
+		testDateCommand("18 October 15", "18/10/2015");
 	}
 	
 	@Test
 	public void test15(){
-		testDateCommand("18 Oct, 93", "18/10/1993");
+		testDateCommand("18 Oct, 14", "18/10/2014");
 	}
 	
 	@Test
 	public void test16(){
-		testDateCommand("18 October, 93", "18/10/1993");
+		testDateCommand("18 October, 14", "18/10/2014");
 	}
 	
 	@Test
 	public void test17(){
-		testDateCommand("18-Oct-93", "18/10/1993");
+		testDateCommand("18-Oct-15", "18/10/2015");
 	}
 	
 	@Test
 	public void test18(){
-		testDateCommand("18-October-93", "18/10/1993");
+		testDateCommand("18-October-15", "18/10/2015");
 	}
 	
 	@Test
 	public void test19(){
-		testDateCommand("18 Oct,93", "18/10/1993");
+		testDateCommand("18 Oct,15", "18/10/2015");
 	}
 	
 	@Test
 	public void test20(){
-		testDateCommand("18 October,93", "18/10/1993");
+		testDateCommand("18 October,14", "18/10/2014");
 	}
 	
 	@Test
 	public void test21(){
-		testDateCommand("18 10 , 93", "18/10/1993");
+		testDateCommand("18 10 , 14", "18/10/2014");
 	}
 	
 	@Test
 	public void test22(){
-		testDateCommand("18 10, 93", "18/10/1993");
+		testDateCommand("18 10, 16", "18/10/2016");
 	}
 	
 	@Test
-	public void test32(){
+	public void test23(){
 		testDateCommand("18/10/2015", "18/10/2015");
 	}
 	
 	@Test
-	public void test33(){
+	public void test24(){
 		testDateCommand("18-10-2015", "18/10/2015");
 	}
 	
 	@Test
-	public void test34(){
+	public void test25(){
 		testDateCommand("18.10.2015", "18/10/2015");
 	}
 	
 	@Test
-	public void test36(){
+	public void test26(){
 		testDateCommand("18Oct15", "18/10/2015");
 	}
 	
 	@Test
-	public void test38(){
+	public void test27(){
 		testDateCommand("1 6 15", "01/06/2015");
 	}
 	
 	@Test
-	public void test39(){
+	public void test28(){
 		testDateCommand("1 Jun 15", "01/06/2015");
 	}
 	
 	@Test 
-	public void test40(){
+	public void test29(){
 		testDateCommand("1.6.15", "01/06/2015");
 	}
 	
 	@Test
-	public void test41(){
+	public void test30(){
 		testDateCommand("1.6.2015", "01/06/2015");
+	}
+	
+	@Test
+	public void test31(){
+		testDateCommand("1/6", "01/06/2014");
+	}
+	
+	@Test
+	public void test32(){
+		testDateCommand("1-6", "01/06/2014");
+	}
+	
+	@Test
+	public void test33(){
+		testDateCommand("1.6", "01/06/2014");
+	}
+	
+	@Test
+	public void test34(){
+		testDateCommand("1 6", "01/06/2014");
+	}
+	
+	@Test
+	public void test35(){
+		testDateCommand("June 1", "01/06/2014");
+	}
+	
+	@Test
+	public void test36(){
+		testDateCommand("1 Jun", "01/06/2014");
+	}
+	
+	@Test
+	public void test37(){
+		testDateCommand("1Jun", "01/06/2014");
+	}
+	
+	@Test
+	public void test38(){
+		testDateCommand("Jun1", "01/06/2014");
+	}
+	
+	@Test
+	public void test39(){
+		testDateCommand("01 12", "01/12/2014");
+	}
+	
+	@Test
+	public void test40(){
+		testDateCommand("1/06", "01/06/2014");
+	}
+	
+	@Test
+	public void test41(){
+		testDateCommand("1-06", "01/06/2014");
+	}
+	
+	@Test
+	public void test42(){
+		testDateCommand("1.06", "01/06/2014");
+	}
+	
+	@Test
+	public void test43(){
+		testDateCommand("1 06", "01/06/2014");
+	}
+	
+	@Test
+	public void test44(){
+		testDateCommand("01/6", "01/06/2014");
+	}
+	
+	@Test
+	public void test45(){
+		testDateCommand("01-6", "01/06/2014");
+	}
+	
+	@Test
+	public void test46(){
+		testDateCommand("01.6", "01/06/2014");
+	}
+	
+	@Test
+	public void test47(){
+		testDateCommand("01 6", "01/06/2014");
+	}
+	
+	@Test
+	public void test48(){
+		testDateCommand("June 01", "01/06/2014");
+	}
+	
+	@Test
+	public void test49(){
+		testDateCommand("01 Jun", "01/06/2014");
+	}
+	
+	@Test
+	public void test50(){
+		testDateCommand("01Jun", "01/06/2014");
+	}
+	
+	@Test
+	public void test51(){
+		testDateCommand("Jun01", "01/06/2014");
+	}
+	
+	@Test
+	public void test52(){
+		testDateCommand("01/06", "01/06/2014");
+	}
+	
+	@Test
+	public void test53(){
+		testDateCommand("01-06", "01/06/2014");
+	}
+	
+	@Test
+	public void test54(){
+		testDateCommand("01.06", "01/06/2014");
+	}
+	
+	@Test
+	public void test55(){
+		testDateCommand("01 06", "01/06/2014");
 	}
 	
 	@Test
@@ -250,23 +375,28 @@ public class TestDateParser {
 	}
 	
 	@Test
-	public void test28(){
-		testDateCommand("93/10/Oct", "10/10/1993");
+	public void invalid28(){
+		testInvalidDateCommand("16/10/Oct", "Not a valid date");
 	}
 	
 	@Test
-	public void test29(){
-		testDateCommand("93/10/oct", "10/10/1993");
+	public void invalid29(){
+		testInvalidDateCommand("15/10/oct", "Not a valid date");
 	}
 	
 	@Test
-	public void test30(){
-		testDateCommand("93.18.10", "18/10/1993");
+	public void invalid30(){
+		testInvalidDateCommand("35.18.10", "Not a valid date");
 	}
 	
 	@Test
-	public void test31(){
-		testDateCommand("93 18 10", "18/10/1993");
+	public void invalid31(){
+		testInvalidDateCommand("49 18 10", "Not a valid date");
+	}
+	
+	@Test
+	public void invalid32(){
+		testInvalidDateCommand(null, "Not a valid date");
 	}
 	
 	
