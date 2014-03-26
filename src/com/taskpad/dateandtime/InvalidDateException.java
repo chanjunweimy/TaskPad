b6859b337a8a13fb98e2249e98845fb8138ff555
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-26 16:33:55 +0800

    Commit

--- src/com/taskpad/dateandtime/InvalidDateException.java
+++ src/com/taskpad/dateandtime/InvalidDateException.java
@@ -1,5 +1,9 @@
 package com.taskpad.dateandtime;
 
+import java.util.logging.Logger;
+
+import com.taskpad.ui.GuiManager;
+
 /**
  * InvalidDateException thrown while it is not a valid date format
  * @author Jun
@@ -11,8 +15,13 @@ public class InvalidDateException extends Exception{
 	 */
 	private static final long serialVersionUID = 8886449578429827179L;
 	
+	private static Logger _logger = Logger.getLogger("TaskPad");
+	
 	public InvalidDateException(){
-		super ();
+		super();
+		GuiManager.callOutput("Not a supported date format");
+		_logger.info("Not a supported date format");
+		//super ();
 	}
 	
 	public InvalidDateException(String Message){
