39fec7032b11da75352c7a4c2ca6913224bea2c9
Lynnette Ng <lynnette-ng@outlook.com>
2014-03-26 17:37:43 +0800

    DateTime package exceptions

--- src/com/taskpad/dateandtime/NullTimeValueException.java
+++ src/com/taskpad/dateandtime/NullTimeValueException.java
@@ -1,22 +1,32 @@
 package com.taskpad.dateandtime;
 
+import java.util.logging.Logger;
+
 /**
  * 
- * @author Jun
+ * @author Jun & Lynnette
  * NullTimeValueException: an exception thrown
  * when user did not key in a time value
  */
 public class NullTimeValueException extends Exception{
+	
 	/**
 	 * generated
 	 */
 	private static final long serialVersionUID = 1045316937520580318L;
+	
+	private static final String MESSAGE = "Error: Please enter a time value";
+	
+	protected static Logger _logger = Logger.getLogger("TaskPad");
+
 
 	public NullTimeValueException(){
-		super ();
+		super (MESSAGE);
+		_logger.info(MESSAGE);
 	}
 	
 	public NullTimeValueException(String Message){
-		super (Message);
+		super (MESSAGE);
+		_logger.info(MESSAGE);
 	}
 }
