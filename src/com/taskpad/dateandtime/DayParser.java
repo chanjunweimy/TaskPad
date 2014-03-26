18f37622e1d8f535a6fd31d739ba3515c079d658
Chan Jun Wei <chanjunweimy@gmail.com>
2014-03-26 17:37:54 +0800

    implementing DayParser and SpecialWordParser~

--- src/com/taskpad/dateandtime/DayParser.java
+++ src/com/taskpad/dateandtime/DayParser.java
@@ -11,17 +11,22 @@ import java.util.Map;
  */
 public class DayParser {
 
+	private static final String SPACE = " ";
+
 	private static final String DAY_INVALID = "Not a valid day";
 	
 	private static Map<String, Integer> _mapWeek = new HashMap<String, Integer>();
 	
-	//private static final String DAY_TODAY = "today";
+	
 	//private static final String DAY_YESTERDAY = "yesterday";
-	//private static final String DAY_TOMORROW = "tomorrow";
-	//private static final String DAY_NOW = "now";
+	//private static final String DAY_TOMORROW = "tomorrow"; 
+	private static final String[] DAY_TODAY = {
+		"today", 
+		"tdy",
+		"now"
+	};
 	
-	
-	private static final String[] _dayInWeek = {
+	private static final String[] DAY_IN_WEEK = {
 		"sunday", 
 		"monday",
 		"tuesday",
@@ -46,8 +51,8 @@ public class DayParser {
 	}
 	
 	private void initializeMapWeek() {		
-		for (int i = 0; i < _dayInWeek.length; i++){
-			_mapWeek.put(_dayInWeek[i], i % 7);
+		for (int i = 0; i < DAY_IN_WEEK.length; i++){
+			_mapWeek.put(DAY_IN_WEEK[i], i % 7);
 		}
 	}
 
@@ -60,9 +65,32 @@ public class DayParser {
 	 * parseDayToDate: to get the date of the day
 	 * @param input String
 	 * @return String
+	 * @throws InvalidDayException 
 	 */
-	protected String parseDayToDate(String input){
-		return null;
+	protected String parseDayToDate(String input) throws InvalidDayException{
+		DateAndTimeManager datm = DateAndTimeManager.getInstance();
+		
+		for (String todayVariation : DAY_TODAY){
+			if (todayVariation.equals(input)){
+				return datm.getTodayDateAndTime();
+			}
+		}
+		
+		SpecialWordParser swp = SpecialWordParser.getInstance();
+		String specialDay = null;
+
+		String[] analyzes = input.split(SPACE);
+		int len = analyzes.length;
+
+		int userDay = parseDayToInt(analyzes[len - 1]);
+
+		boolean isDay = userDay >= 0 && userDay < 7;
+		if (isDay){
+			specialDay = input.substring(input.lastIndexOf(analyzes[len - 1])).trim();
+			specialDay = swp.parseSpecialDay(specialDay, userDay);
+		}
+		
+		return specialDay;
 	}
 	
 	/**
--- src/com/taskpad/dateandtime/SpecialWordParser.java
+++ src/com/taskpad/dateandtime/SpecialWordParser.java
@@ -2,6 +2,7 @@ package com.taskpad.dateandtime;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * SpecialWordParser: parses special words like next and previous
@@ -11,17 +12,21 @@ import java.util.Map;
  */
 public class SpecialWordParser {
 	
+	private static final int DAY_WEEK = 7;
+
+	private static final String EMPTY = "";
+
 	//private static Map<Integer, String[]> _specialWordMap = new HashMap<Integer, String[]>();
 	private static Map<String, Integer> _specialWordMap = new HashMap<String, Integer>();
 	
 	private static SpecialWordParser _specialWordParser = new SpecialWordParser();
 	
 	private static final String[] _nextMap = {
-		"NEXT", "NXT", "FOLLOWING", "COMING"
+		"NEXT", "NXT", "FOLLOWING", "COMING", "TOMORROW"
 	};
 	
 	private static final String[] _prevMap = {
-		"PREVIOUS", "PREV", "PAST", "LAST"
+		"PREVIOUS", "PREV", "PAST", "LAST", "YESTERDAY"
 	};
 	
 	private static final String[] _thisMap = {
@@ -36,15 +41,76 @@ public class SpecialWordParser {
 		return _specialWordParser;
 	}
 	
+	protected String parseSpecialDay(String specialDay, int userDay){
+		DateAndTimeManager datm = DateAndTimeManager.getInstance();
+		DayParser dp = DayParser.getInstance();
+		TimeWordParser twp = TimeWordParser.getInstance();
+		
+		String todayDay = datm.getTodayDay();
+		int todayDayStat = getTodayDaye(dp, todayDay);
+		int nxt = 1;
+		
+		if (specialDay == SpecialWordParser.EMPTY){
+			return getNextDay(userDay, twp, todayDayStat, nxt);
+		}
+		
+		specialDay = specialDay.toUpperCase();
+		
+		Scanner sc = new Scanner(specialDay);
+		
+		while (sc.hasNext()){
+			String specialToken = sc.next();
+			
+			nxt += _specialWordMap.get(specialToken).intValue();
+		}
+		sc.close();
+		
+
+		return null;
+	}
+
+	/**
+	 * @param userDay
+	 * @param twp
+	 * @param todayDayStat
+	 */
+	private String getNextDay(int userDay, TimeWordParser twp, int todayDayStat, int nxt) {
+		todayDayStat -= userDay;
+		
+		if (todayDayStat < 0){
+			todayDayStat += DAY_WEEK;
+		}
+		
+		nxt--;
+		todayDayStat += DAY_WEEK * nxt;
+		
+		try {
+			return twp.timeWord(todayDayStat + "d");
+		} catch (NullTimeUnitException | NullTimeValueException e) {
+			assert (false);
+		}
+		return null;
+	}
+
+	private int getTodayDaye(DayParser dp, String todayDay) {
+		int todayDayStat = 0;
+		try {
+			todayDayStat = dp.parseDayToInt(todayDay);
+		} catch (InvalidDayException e) {
+			assert (false);
+		}
+		return todayDayStat;
+	}
+	
 	/**
 	 * parseSpecialWord parses special sentence like
-	 * next next Monday.
+	 * next next hour.
 	 * 
 	 * It can solve types like 
-	 * next next Monday
-	 * (special words... + DayWord/TimeUnit)
+	 * next next hour
+	 * (special words... + TimeUnit)
 	 * 
-	 * or 
+	 * or
 	 * 
 	 * next next 1 hour 
 	 * (sepcial words... + integer + TimeUnit)
@@ -55,9 +121,8 @@ public class SpecialWordParser {
 	 * @param specialWord String
 	 * @return int
 	 */
-	protected int parseSpecialWord(String specialWord){
-		int res = 0;
-		return res;
+	protected String parseSpecialWord(String specialWord, boolean multiply, int num){
+		return null;
 	}
 	
 	private void initialiseSpecialWordMap() {
