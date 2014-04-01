fa98643895a3b673672d3481b0846e46b74da72b
Chan Jun Wei <chanjunweimy@gmail.com>
2014-04-01 20:08:44 +0800

    implemented createDesc() and parseNumber() in DateAndTimeRetriever

--- src/com/taskpad/dateandtime/DateAndTimeRetriever.java
+++ src/com/taskpad/dateandtime/DateAndTimeRetriever.java
@@ -1,5 +1,7 @@
 package com.taskpad.dateandtime;
 
+import java.util.Scanner;
+
 /**
  * This class is for us to find the existence of Date and Time in an input String
  * 
@@ -91,7 +93,15 @@ public class DateAndTimeRetriever {
 	 */
 
 	protected static String formatDateAndTimeInString(String input) {
-		//step one: convert all number words to numbers using number parser
+		DateAndTimeManager datmParser = DateAndTimeManager.getInstance();
+		
+		
+		String desc = createDesc(input);
+		
+		//step one: convert all number words to numbers using number parser		
+		String numberedInput = parseNumber(input, datmParser);
+		
+		
 		//step two: find holiday words and replace with date
 		//input = HolidayDates.getInstance().replaceHolidayDate(input);
 		//step three: find dayParser words and find words before (i.e. next/prev) and replace with date
@@ -101,6 +111,52 @@ public class DateAndTimeRetriever {
 		
 		//return that string to parse in respective Add/Addrem/Alarm classes - already done with return input
 		return input;
+	}
+
+	/**
+	 * @param input
+	 * @param datmParser
+	 */
+	private static String parseNumber(String input, DateAndTimeManager datmParser) {
+		Scanner sc = new Scanner(input);
+		StringBuffer changedString = new StringBuffer();
+		StringBuffer numberString = new StringBuffer();
+		boolean isNumberContinue = false;
+		while (sc.hasNext()){
+			String token = sc.next();
+			if (!datmParser.isNumber(token)){
+				if (isNumberContinue){
+					String realNumber = datmParser.parseNumber(numberString.toString().trim());
+					changedString.append(realNumber + " ");
+					numberString = new StringBuffer();
+				}
+				
+				changedString.append(token + " ");
+				isNumberContinue = false;
+			} else {
+				if (!isNumberContinue){
+					isNumberContinue = true;
+				}
+				numberString.append(token + " ");
+			}
+		}
+		sc.close();
+		return changedString.toString().trim();
+	}
+
+	/**
+	 * @param input
+	 */
+	private static String createDesc(String input) {
+		String desc = input.trim();
+		if (!desc.startsWith("\"")){
+			desc = "\"" + desc;
+		}
+		
+		if (!input.endsWith("\"")){
+			desc = desc + "\"";
+		}
+		return desc;
 	}
 
 	
