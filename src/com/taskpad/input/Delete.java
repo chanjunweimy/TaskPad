5e44ef28fe2c25d3c9a43442342730f4ef0ad5a5
Lynnette Ng <lynnette-ng@outlook.com>
2014-04-05 15:45:12 +0800

    Delete can delete by keyword now

--- src/com/taskpad/input/Delete.java
+++ src/com/taskpad/input/Delete.java
@@ -13,9 +13,11 @@ public class Delete extends Command{
 	
 	private static String COMMAND_DELETE = "DELETE";
 	private static String PARAMETER_TASK_ID = "TASKID";
+	private static String PARAMETER_KEYWORD = "KEYWORD";
 	private static int NUMBER_ARGUMENTS = 1;		//Number of arguments for delete
-	@SuppressWarnings("unused")
-	private static String TEST = "TESTING CONFLICTS";
+	
+	private String _taskID;
+	private String _keyword;
 
 	public Delete(String input, String fullInput) {
 		super(input, fullInput);	
@@ -25,22 +27,51 @@ public class Delete extends Command{
 	protected void initialiseOthers(){
 		setNUMBER_ARGUMENTS(NUMBER_ARGUMENTS);
 		setCOMMAND(COMMAND_DELETE);	
+		
+		_taskID = "";
+		_keyword = "";
 	}
 	
 	@Override
 	protected boolean commandSpecificRun() {
-		putInputParameters();
+		if (isTaskID(input)){
+			_taskID = input.trim();
+		} else{
+			_keyword = input.trim();
+		}
+		
 		return true;
+	}
+	
+	private boolean isTaskID(String input){
+		try{
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException e){
+			return false;
+		}
 	}
 
 	@Override
 	protected void initialiseParametersToNull() {
 		inputParameters.put(PARAMETER_TASK_ID, "");		
+		inputParameters.put(PARAMETER_KEYWORD, "");
 	}
 
 	@Override
 	protected void putInputParameters() {
-		putOneParameter(PARAMETER_TASK_ID, input.trim());
+		putOneParameter(PARAMETER_TASK_ID, _taskID);
+		putOneParameter(PARAMETER_KEYWORD, _keyword);
+	}
+	
+	protected boolean checkIfIncorrectArguments() throws TaskIDException, InvalidParameterException{
+		String inputString[] = input.split(" ");
+		
+		if (isNotNumberArgs(inputString)){
+			throw new InvalidParameterException();
+		}
+		
+		return false;
 	}
 	
 }
