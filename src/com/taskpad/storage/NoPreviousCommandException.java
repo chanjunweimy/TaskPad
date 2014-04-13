b1b4d5c3a6de9628c0a0907dbc3557a66a480ccc
Chan Jun Wei <chanjunweimy@gmail.com>
2014-04-13 19:11:45 +0800

    fixing addrem's non-delimiters

---
+++ src/com/taskpad/storage/NoPreviousCommandException.java
@@ -0,0 +1,10 @@
+//@author A0105788U
+
+
+package com.taskpad.storage;
+
+public class NoPreviousCommandException extends Exception {
+	public NoPreviousCommandException() {
+		System.out.println("No previous command.");
+	}
+}
