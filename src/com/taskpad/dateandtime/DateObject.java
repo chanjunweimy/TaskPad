5ed0ed8cc71bc8aced02465e76f51106cac90411
Wang TaiNing <tainingw@gmail.com>
2014-04-13 19:09:36 +0800

    corrected reminder

---
+++ src/com/taskpad/dateandtime/DateObject.java
@@ -0,0 +1,26 @@
+//@author A0119646X
+
+package com.taskpad.dateandtime;
+
+/**
+ * Date object to contain parsed date and the input date format
+ * @deprecated
+ */
+public class DateObject {
+	
+	private String parsedDate;
+	private String inputDate;
+	
+	public DateObject(String parsedDate, String inputDate){
+		this.parsedDate = parsedDate;
+		this.inputDate = inputDate;
+	}
+
+	public String getParsedDate() {
+		return parsedDate;
+	}
+
+	public String getInputDate() {
+		return inputDate;
+	}
+}
